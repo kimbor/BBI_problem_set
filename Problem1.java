e510b479dfd62b5bb67d9341abee813b388f19a1
Kim <Kim@DESKTOP-NVFVIO8>
2020-10-13 21:44:52 -0700

    Got it working against a file

--- Problem1.java
+++ Problem1.java
@@ -9,21 +9,21 @@ in the input are fragmented into pieces of equal length L; each piece is then se
 technology, and its content is encoded in the output. The particular encoding used in the output
 is the following:
 
-● The file contains multiple consecutive entries, one per piece.
-● Each piece is represented by L consecutive bytes (1 byte = 8 bits).
-● The first two (most significant) bits of each byte encode the DNA letter:
+* The file contains multiple consecutive entries, one per piece.
+* Each piece is represented by L consecutive bytes (1 byte = 8 bits).
+* The first two (most significant) bits of each byte encode the DNA letter:
 00 represents A
 01 represents C
 10 represents G
 11 represents T
-● The last six (least significant) bits of each byte encode the confidence that the readout
+* The last six (least significant) bits of each byte encode the confidence that the readout
 was correct, also known as the quality score. It is represented as an unsigned 6-bit
 integer in the range 0 to 63.
 
 Write a program that takes as input an encoded file as well as the number L, and converts it to a
 text file of the following format (known as the FASTQ format):
 
-● Each piece is represented by four lines:
+* Each piece is represented by four lines:
 - The first line contains the word @READ_ followed by the piece index. The first
 piece has an index of 1, so its first line would be @READ_1
 - The second line contains L characters in the {A,C,G,T} alphabet, representing
@@ -33,7 +33,7 @@ the DNA sequence of the piece.
 - The fourth line contains L characters, representing the quality scores of the
 piece. Each score is represented as an ASCII character in the range 33-96, by
 adding 33 to the original score. For example, if the original score is 0, it should be
-represented by the ASCII character 33 (“!”)
+represented by the ASCII character 33 ("!")
 
 Example input (for L=2) shown here in binary:
 00000000 11100000 11000001 01111111
@@ -51,7 +51,7 @@ TC
 import java.io.*;
 import java.util.*;
 
-class Problem1 {
+public class Problem1 {
   static final String[] DNA_BASE_ENCODINGS = {"00", "01", "10", "11"};
   static final String[] DNA_BASES = {"A", "C", "G", "T"};
 
@@ -66,9 +66,9 @@ class Problem1 {
     } // inputStream resource automagically closed
 
     // test example reading from a file
-    String filename = "test.txt";
-    try (InputStream inputStream = new FileInputStream(filename)) {
-      System.out.println(toFASTQ(inputStream, 3));
+    String filename = "problem1.txt";
+    try (InputStream inputStream = new FileInputStream(filename)) {  // java 7 resource declaration syntax
+      System.out.println(toFASTQ(inputStream, 4));
     } catch (IOException io) {
         System.out.println("Error reading from file: " + io.getMessage());
     } // resources automagically closed
@@ -86,6 +86,7 @@ class Problem1 {
     int pieceIndex = 1; 
 
     String nextByte = getNextByte(inputStream);
+System.out.println(nextByte);    
     while (!nextByte.trim().equals("")) {
       output.append("@READ_" + pieceIndex + "\n");
       StringBuffer qualityOutput = new StringBuffer();;
